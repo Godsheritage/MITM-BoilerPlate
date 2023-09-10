50c999cf1a3c085c3adb2039fe1e79e3b4ace0ad
Godsheritage  Adeoye <Adeoyegodsheritage@gmail.com>
2023-09-09 20:44:16 -0400

    fixed bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { theme } from "antd";
 import Home from "./views/Home.tsx";
 import Header from "./components/Header.tsx";
-import FooterComponent from "./components/footer.tsx";
-// import Footer from "./components/Footer.tsx";
+import FooterComponent from "./components/Footer.tsx";
 import Education from "./views/Education.tsx";
 import FormView from "./views/FormView.tsx";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// const { Header, Content, Footer } = Layout;
+
+
+
 const App: React.FC = () => {
   const {
     token: { colorBgContainer },
